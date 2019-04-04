22a2ad6269e7ebba1b2fa45c93d51cd42c6089b8
gabriel96744 <42453519+gabriel96744@users.noreply.github.com>
2019-04-04 13:58:08 -1000

    Add files via upload

--- barrier.js
+++ barrier.js
@@ -1,9 +1,9 @@
 function barrier(){
-  this.leftPull = 1;
-  this.velocity = 5;
+  this.leftPull = 0.4;
+  this.velocity = 0;
   this.width = 100;
   this.height = 100;
- this.y = Math.floor((Math.random()*h)-50);
+ this.y = Math.floor((Math.random()*h)-100);
   this.x = w + this.width;
   this.show = function(){
     this.x;
@@ -14,6 +14,11 @@ function barrier(){
   this.update = function(){
     this.velocity += this.leftPull;
     this.x -= this.velocity;
+    if(this.x < 0 - this.width){
+      this.velocity = 0;
+      this.y = Math.floor((Math.random()*h)-100);
+  this.x = w + this.width;
+    }
   }
   
 }
