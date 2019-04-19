75e61abcf5f81932741b6b5111c844c07066f828
gabriel96744 <42453519+gabriel96744@users.noreply.github.com>
2019-04-18 22:26:12 -1000

    update

--- platform.js
+++ platform.js
@@ -1,13 +1,12 @@
 function platform(){
-  this.x = 500;
-  this.y = 500;
+  this.x = 0;
+  this.y = h+100;
   this.height = 100;
-  this.width = 100;
+  this.width = w;
 
   this.show = function(){
 
-    
-  fill(color("green"));
+    (fill(color("tan")));
   rect(this.x,this.y,this.height,this.width);
 };
   
